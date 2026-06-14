b54e0b5f1e3602fcbaf91424404edcce3bdf1761
patilkiran0602-glitch <patilkiran0602@gamil.com>
2026-06-14 20:22:49 +0530

    structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="facial_emotion_detection_system",
+    version="0.1.0",
+    author="kiran",
+    author_email="patilkiran0602@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
